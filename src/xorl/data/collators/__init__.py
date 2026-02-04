c11b284b49bb6790a8a5d75fd674658d24f8d35e
qywu <qingyang@together.ai>
2026-02-03 16:21:34 -0800

    Add ShiftTokensCollator and fix loss function return values

--- src/xorl/data/collators/__init__.py
+++ src/xorl/data/collators/__init__.py
@@ -6,6 +6,7 @@ from .packing_concat_collator import (
     add_flash_attention_kwargs_from_position_ids,
 )
 from .sequence_shard_collator import TextSequenceShardCollator
+from .shift_tokens_collator import ShiftTokensCollator
 from .stream_distill_collator import StreamDistillDataCollator
 from .tensor_collator import ToTensorCollator
 
@@ -15,6 +16,7 @@ __all__ = [
     "FlattenCollator",
     "PackingConcatCollator",
     "add_flash_attention_kwargs_from_position_ids",
+    "ShiftTokensCollator",
     "TextSequenceShardCollator",
     "StreamDistillDataCollator",
     "ToTensorCollator",
